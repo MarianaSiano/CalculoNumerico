c8f5a44dda7e9423db0720f7acea1ffb6faebed3
MarianaSiano <marianasianop@gmail.com>
2025-10-30 16:18:03 -0300

    Melhoeando o codigo

--- Taylor.py
+++ Taylor.py
@@ -1,17 +1,17 @@
 def exp_taylor(n, x):
     fat = 1.0
     term = 1.0
-    sum = term
+    soma = term
     i = 1
     while i <= n:
         fat = fat * i
         term = term * x
-        sum = sum + term / fat
+        soma = soma + term / fat
         i = i + 1
-    return sum
+    return soma
 
 if __name__ == "__main__":
     n = int(input("Digite n => "))
-    x = int(input("Digite x => "))
+    x = float(input("Digite x => ")) # Permitir valores flutuantes para x
 
-    print (exp_taylor(n, x))
+    print(exp_taylor(n, x))
